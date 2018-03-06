55060a515be8b5aa6938211a89e42468c67b67dd
cgewecke <christophergewecke@gmail.com>
2018-03-05 19:19:06 -0800

    Add coverage

--- contracts/ProcessRunner.sol
+++ contracts/ProcessRunner.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.18;
 
-interface ProcessRunner {
+contract ProcessRunner {
   function performStateTransition(bytes32 machineId, bytes32 processId, bytes32 toState) returns (bool);
 }
